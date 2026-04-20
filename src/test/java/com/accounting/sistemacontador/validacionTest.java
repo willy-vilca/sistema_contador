5710e4841e55c306ae0c4b6c50044f8153fd4868
Tu nombre <tu@email.com>
2026-04-20 01:53:12 -0500

    Se agregaron pruebas unitarias

---
+++ src/test/java/com/accounting/sistemacontador/validacionTest.java
@@ -0,0 +1,28 @@
+package com.accounting.sistemacontador;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class validacionTest {
+    @Test
+    public void testValidDNI() {
+
+        String dni = "12345678";
+
+        boolean valid = dni.matches("\\d{8}");
+
+        assertTrue(valid);
+    }
+
+    @Test
+    public void testInvalidDNI() {
+
+        String dni = "123";
+
+        boolean valid = dni.matches("\\d{8}");
+
+        assertFalse(valid);
+    }
+}
+
+
